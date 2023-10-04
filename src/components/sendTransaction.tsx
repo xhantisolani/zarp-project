234be4b2f49ef54f18c377ca00a2f6909e1acc18
xhantisolani <solanixhanti@gmail.com>
2023-10-04 20:33:42 +0200

    this app uses the signer from the wallet to send the transaction

--- src/components/sendTransaction.tsx
+++ src/components/sendTransaction.tsx
@@ -168,30 +168,49 @@ export function SendTransaction() {
 
         // Convert the amount to Wei
         const amountInWei = ethers.utils.parseEther(amount);
-
+        const signer = provider.getSigner();
+        
         const transactionRequest = {
           to: to, //  recipient's Ethereum address
           value: amountInWei, //  amount to send in Ether
         };
 
-        sendTransaction(transactionRequest);
-       
+        tx = await signer.sendTransaction(transactionRequest)
+
+        .then((tx) => {
+          openErrorModal(`Transaction hash: ${tx.hash}`);
+          return tx.wait(); // Wait for confirmation
+        })
+        .then((receipt) => {
+          openErrorModal(`Transaction confirmed in block ${receipt.blockNumber}`);
+        })
+        .catch((error) => {
+          openErrorModal(`Transaction error: ${error}`);
+        });
       } else {
         // Send token transaction
         const tokenContract = new ethers.Contract(
           selectedToken.address,
           ERC20_ABI,
-          signer
+          provider
         );
   
         // Convert the amount to the appropriate token units (e.g., wei for ERC-20 with 18 decimals)
         const amountInTokenUnits = ethers.utils.parseUnits(amount, selectedToken.decimals);
   
-        tx = await tokenContract.transfer(to, amountInTokenUnits);
+        tx = await tokenContract.transfer(to, amountInTokenUnits)
+        
+        .then((result: any) => {
+          openErrorModal(`Function result: ${result}`);
+        })
+        .catch((error: any) => {
+          openErrorModal(`Error calling function: ${error}`);
+        });
       }
   
       setIsSuccess(true);
       setTransactionHash(tx.hash);
+
     } catch (error) {
       openErrorModal('Error sending transaction:');
     } finally {
@@ -232,7 +251,8 @@ export function SendTransaction() {
         aria-label="Amount (ether)"
         onChange={(e) => {
         setAmount(e.target.value);
-        getGasEstimate(selectedToken as Token, amount, to);
+        getGasEstimate(selectedToken as Token, e.target.value, to);
+
         }}
         placeholder="0.00"
         value={amount}
@@ -264,14 +284,14 @@ export function SendTransaction() {
      
       {gas != null && (  <div className={styles.label}>Gas: {gas}</div>)}
 
-      {isSuccess && (
+      {transactionHash && (
         <div className={styles.label}>
-          Successfully sent {amount} {selectedToken?.name} {to}
+              Successfully sent {amount} {selectedToken?.name} {to}
           <div>
-            <a href={`https://etherscan.io/tx/${transactionHash}`}>Etherscan</a>
-          </div>
-        </div>
-      )}
+           <a href={`https://etherscan.io/tx/${transactionHash}`} target="_blank" rel="noopener noreferrer">  View on Etherscan </a>
+         </div>
+        </div> )}
+
        {/* Render the ErrorModal component */}
        <ErrorModal isOpen={isErrorModalOpen} onClose={closeErrorModal} error={error} />
       </div>
