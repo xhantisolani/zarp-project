6cd836b98cc4200f71fb5614c0a22452e0bff3b1
rowancowper <134603409+rowancowper@users.noreply.github.com>
2023-10-08 18:32:44 +0200

    Changed ENS to ETH

--- src/components/sendTransaction.tsx
+++ src/components/sendTransaction.tsx
@@ -161,7 +161,7 @@ export function SendTransaction() {
       const signer = provider.getSigner();
         
       let tx;
-      if (selectedToken.name === 'Ethereum Name Service') {
+      if (selectedToken.name === 'Ethereum') {
         // Send Ether transaction
 
         // Convert the amount to Wei
